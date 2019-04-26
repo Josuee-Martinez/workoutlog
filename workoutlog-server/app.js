1f1716338e07ef1d819f0226402fbc72a8bb27be
Josue Martinez <josue@Josues-MBP.hsd1.in.comcast.net>
2019-04-25 21:57:02 -0400

    new

---
+++ workoutlog-server/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+var express = require('express');
+var app = express();
+var api = require('./controllers/apicontroller');
+var log = require('./controllers/apilogcontroller');
+var sequelize = require('./db');
+var bodyParser = require('body-parser');
+
+sequelize.sync();
+
+app.use(bodyParser.json());
+app.use(require('./middleware/headers'));
+app.use('/api', api);
+
+app.use(require('./middleware/validate-session'));
+
+app.use('/api/log', log);
+
+app.listen(3000, function(){
+    console.log('app listening on port 3000');
+})
